8fbf122d3dd31fd0db5edb95632ebf7637ab9604
jack-skerrett-bluefruit <jack.skerrett@bluefruit.co.uk>
2018-04-19 09:03:23 +0100

    Initial commit

---
+++ AccountManagement/AccountManagement/Class1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement
+{
+    public interface IAccount
+    {
+        void PayInFunds(decimal amount);
+        bool WithdrawFunds(decimal amount);
+        decimal GetBalance();
+        string GetName();
+        int GetAccountNumber();
+        bool SetName(string inName);
+        bool Save(string filename);
+        void Save(System.IO.TextWriter textOut);
+    }
+
+    public class CustomerAccount : IAccount
+    {
+        public CustomerAccount(
+            string newName,
+            decimal initialBalance)
+        {
+            name = newName;
+            balance = initialBalance;
+        }
+        private decimal balance = 0;
+        private string name;
+        private int accountNumber = UniqueAccountNumber();
+        private static List<int> accountNumbers = new List<int>();
+
+
+        public virtual bool WithdrawFunds(decimal amount)
+        {
+            if (balance < amount)
+            {
+                return false;
+            }
+            balance = balance - amount;
+            return true;
+        }
+
+        public void PayInFunds(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetAccountNumber()
+        {
+            return accountNumber;
+        }
+        public static decimal ValidateDecimal(string prompt, int min, int max)
+        {
+            if (min >= max)
+            {
+                //some kind of exception to be thrown about your min and max feels being all kinds of fucked up
+            }
+            decimal userValue;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                try
+                {
+                    userValue = decimal.Parse(Console.ReadLine());
+                    if (userValue < min || userValue > max)
+                    {
+                        Console.WriteLine("Your value must fall between {0} (min) and {1} (max)", min, max);
+                        continue;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("You must enter a numeric value between {0} (min) and {1} (max)", min, max);
+                }
+            }
+            return userValue;
+
+        }
+        public static string ValidateName(string name)
+        {
+            string trimmedName = name.Trim();
+            while (true)
+            {
+
+                if (name == null)
+                {
+                    Console.WriteLine("\nYou have entered a null value, please enter another name.");
+                    continue;
+                }
+                if (trimmedName.Length == 0)
+                {
+                    Console.WriteLine("\nYou didn't enter any text, please enter a name");
+                    continue;
+                }
+                break;
+            }
+
+            return name;
+        }
+        public bool SetName(string inName)
+        {
+            string reply;
+            reply = ValidateName(inName);
+            if (reply.Length > 0)
+                return false;
+            this.name = inName.Trim();
+            return true;
+        }
+        public static int UniqueAccountNumber()
+        {
+            Random rdn = new Random();
+            int validateAccountNumber;
+            while (true)
+            {
+                validateAccountNumber = rdn.Next(100000, 1000000);
+                if (accountNumbers.Contains(validateAccountNumber))
+                {
+                    continue;
+                }
+                accountNumbers.Add(validateAccountNumber);
+                return validateAccountNumber;
+            }
+
+        }
+        public virtual void Save(System.IO.TextWriter textOut)
+        {
+            textOut.WriteLine(accountNumber);
+            textOut.WriteLine(name);
+            textOut.WriteLine(balance);
+
+        }
+
+        public bool Save(string filename)
+        {
+            System.IO.TextWriter textOut = null;
+            try
+            {
+                textOut = new System.IO.StreamWriter(filename);
+                Save(textOut);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (textOut != null)
+                {
+                    textOut.Close();
+                }
+            }
+            return true;
+        }
+
+        public static CustomerAccount Load(
+            System.IO.TextReader textIn)
+        {
+            CustomerAccount result = null;
+
+            try
+            {
+                int accountNumber = int.Parse(textIn.ReadLine());
+                string name = textIn.ReadLine();
+                string balanceText = textIn.ReadLine();
+                decimal balance = decimal.Parse(balanceText);
+                result = new CustomerAccount(name, balance);
+            }
+            catch
+            {
+                return null;
+            }
+            return result;
+        }
+
+        public static CustomerAccount Load(string filename)
+        {
+            System.IO.TextReader textIn = null;
+            CustomerAccount result = null;
+            try
+            {
+                textIn = new System.IO.StreamReader(filename);
+                result = CustomerAccount.Load(textIn);
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (textIn != null) textIn.Close();
+            }
+
+            return result;
+        }
+
+        public CustomerAccount(System.IO.TextReader textIn)
+        {
+            accountNumber = int.Parse(textIn.ReadLine());
+            name = textIn.ReadLine();
+            string balanceText = textIn.ReadLine();
+            balance = decimal.Parse(balanceText);
+        }
+
+    }
+
+    public class BabyAccount : CustomerAccount
+    {
+        private string parentName;
+        private int maxWithdrawal = 10;
+        public string GetParentName()
+        {
+            return parentName;
+        }
+
+        public override bool WithdrawFunds(decimal amount)
+        {
+            if (amount > maxWithdrawal)
+            {
+                Console.WriteLine("There is a maximum of {0:C} on baby accounts.", maxWithdrawal);
+                return false;
+            }
+            return base.WithdrawFunds(amount);
+        }
+
+        public override void Save(System.IO.TextWriter textOut)
+        {
+            base.Save(textOut);
+            textOut.WriteLine(parentName);
+        }
+
+        public BabyAccount(
+            string newName,
+            decimal initialBalance,
+            string inParentName)
+            : base(newName, initialBalance)
+        {
+            parentName = inParentName;
+        }
+        public BabyAccount(System.IO.TextReader textIn) :
+            base(textIn)
+        {
+            parentName = textIn.ReadLine();
+        }
+    }
+}
---
+++ RobMilesBank/RobMilesBank/Bank.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using AccountManagement;
+
+
+
+namespace RobMilesBank
+{
+    public class DictionaryBank 
+    {
+        Dictionary<int, IAccount> accountDictionary = new Dictionary<int, IAccount>();
+        public IAccount FindAccount(int accountNumber)
+        {
+            if (accountDictionary.ContainsKey(accountNumber) == true)
+                return accountDictionary[accountNumber];
+            else
+                return null;
+        }
+        public bool AccountsInBank()
+        {
+            if (accountDictionary.Count == 0)
+                return false;
+            return true;
+        }
+        public bool StoreAccount(IAccount account)
+        {
+            if (accountDictionary.ContainsKey(account.GetAccountNumber()) == true)
+                return false;
+
+            accountDictionary.Add(account.GetAccountNumber(), account);
+            return true;
+        }
+
+        public void Save(System.IO.TextWriter textOut)
+        {
+            textOut.WriteLine(accountDictionary.Count);
+            foreach (CustomerAccount account in accountDictionary.Values)
+            {
+                textOut.WriteLine(account.GetType().Name);
+                account.Save(textOut);
+            }
+        }
+
+        public bool Save(string filename)
+        {
+            System.IO.TextWriter textOut = null;
+            try
+            {
+                textOut = new System.IO.StreamWriter(filename);
+                Save(textOut);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (textOut != null)
+                {
+                    textOut.Close();
+                }
+            }
+            return true;
+        }
+
+        public static DictionaryBank Load(System.IO.TextReader textIn)
+        {
+            DictionaryBank result = new DictionaryBank();
+            string countString = textIn.ReadLine();
+            int count = int.Parse(countString);
+
+            for (int i = 0; i < count; i++)
+            {
+                string className = textIn.ReadLine();
+                IAccount account =
+                    AccountFactory.MakeAccount(className, textIn);
+                result.StoreAccount(account);
+            }
+            return result;
+        }
+
+        public static DictionaryBank Load(string filename)
+        {
+            System.IO.TextReader textIn = null;
+            DictionaryBank result = null;
+            try
+            {
+                textIn = new System.IO.StreamReader(filename);
+                result = DictionaryBank.Load(textIn);
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (textIn != null) textIn.Close();
+            }
+
+            return result;
+        }
+
+    }
+
+    class AccountFactory
+    {
+        public static IAccount MakeAccount(
+            string name, System.IO.TextReader textIn)
+        {
+            switch (name)
+            {
+                case "CustomerAccount":
+                    return new CustomerAccount(textIn);
+                case "BabyAccount":
+                    return new BabyAccount(textIn);
+                default:
+                    return null;
+            }
+        }
+    }
+    public class AccountCreateUI
+    {
+        private DictionaryBank ourBank;
+        public AccountCreateUI(DictionaryBank inBank)
+        {
+            ourBank = inBank;
+        }
+        public static  void CreateScript(DictionaryBank ourBank)
+        {
+            string continueCreating;
+            do
+            {
+                Console.WriteLine("What type of account would you like to create?" +
+                    "\n      Type customer for a new customer account" +
+                    "\n      Type baby for a new baby account");
+                string input = AccountEditUI.TrimLower(Console.ReadLine());
+                switch (input)
+                {
+                    case "customer":
+                        AccountCreateUI create = new AccountCreateUI(ourBank);
+                        create.CreateCustomerAccount();
+                        break;
+                    case "baby":
+                        create = new AccountCreateUI(ourBank);
+                        create.CreateBabyAccount();
+                        break;
+                    default:
+                        Console.WriteLine("You did not select a baby or customer account.");
+                            break;
+                }
+                continueCreating = AccountEditUI.ThisOrThat("\nWould you like to create an account [Y] or [N]", "You have not entered [Y] or [N]", "y", "n");
+            } while (continueCreating == "y");
+
+        }
+        private void CreateCustomerAccount()
+        {
+            Console.WriteLine("\nWhat would you like the name on the account to be (this can be edited later)?");
+            string name = CustomerAccount.ValidateName(Console.ReadLine());
+            decimal balance = CustomerAccount.ValidateDecimal("How much money would you like initially deposited into the account?", 0, 10000);
+            CustomerAccount newAccount = new CustomerAccount(name, balance);
+            ourBank.StoreAccount(newAccount);
+        }
+        private void CreateBabyAccount()
+        {
+            Console.WriteLine("\nWhat would you like the name on the account to be (this can be edited later)?");
+            string name = CustomerAccount.ValidateName(Console.ReadLine());
+            decimal balance = CustomerAccount.ValidateDecimal("How much money would you like initially deposited into the account?", 0, 1000);
+            Console.WriteLine("What would you like the parent's name on the account to be (this can be edited later)?"); //this cannot be edited later yet
+            string parentName = CustomerAccount.ValidateName(Console.ReadLine());
+            BabyAccount newAccount = new BabyAccount(name, balance, parentName);
+            ourBank.StoreAccount(newAccount);
+        }
+    }
+    public class AccountEditUI
+    {
+        private IAccount account;
+        public AccountEditUI(IAccount inaccount)
+        {
+            account = inaccount;
+        }
+        public void DoEdit(IAccount account)// need to be careful with this because at the minute I can accept a babyaccount to edit but there is no option to edit the baby account specific things (parent name)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("\nEditing account for {0}", account.GetName());
+                Console.WriteLine("    Enter name to edit name");
+                Console.WriteLine("    Enter pay to pay in funds");
+                Console.WriteLine("    Enter draw to draw out funds");
+                Console.WriteLine("    Enter exit to exit editing mode");
+                input = TrimLower(Console.ReadLine());
+                switch (input)
+                {
+                    case "name":
+                        AccountEditUI edit = new AccountEditUI(account);
+                        edit.EditName();
+                        break;
+                    case "pay":
+                        edit = new AccountEditUI(account);
+                        edit.PayInFunds();
+                        break;
+                    case "draw":
+                        edit = new AccountEditUI(account);
+                        edit.WithdrawFunds();
+                        break;
+                }
+            } while (input != "exit");
+        }
+        public static void EditScript(DictionaryBank ourBank)
+        {
+            IAccount chosenAccount;
+            string continueEditing;
+            do
+            {
+                while (true)
+                {
+                    int chosenAccountNumber = Decimal.ToInt32(CustomerAccount.ValidateDecimal("\nWhich account would you like to edit (please give the account number)?", 100000, 1000000));
+                    chosenAccount = ourBank.FindAccount(chosenAccountNumber);
+                    if (chosenAccount == null)
+                    {
+                        Console.WriteLine("\nThat name does not exist in this bank. Are you sure you spelt the name correctly?");
+                        continue;
+                    }
+                    break;
+                }
+
+                AccountEditUI edit = new AccountEditUI(chosenAccount);
+                edit.DoEdit(chosenAccount);
+                continueEditing = ThisOrThat("\nWould you like to continue editing [Y] or [N]? ", "You have not entered [Y] or [N].", "y", "n");
+            } while (continueEditing == "Y");
+        }
+        public void EditName()
+        {
+            string newName;
+            Console.WriteLine("\n-----Name Edit-----");
+            while (true)
+            {
+                Console.WriteLine("Enter new name: ");
+                newName = Console.ReadLine();
+                string reply;
+                reply = CustomerAccount.ValidateName(newName);
+                if (reply.Length == 0)
+                    break;
+                Console.WriteLine("Invalid name: " + reply);
+            }
+            account.SetName(newName);
+        }
+        public void PayInFunds()
+        {
+            decimal depositValue;
+            Console.WriteLine("\n-----Deposit Funds-----");
+            while (true)
+            {
+                depositValue = CustomerAccount.ValidateDecimal("Enter how much money you would like to deposit: ", 0, 10000);
+                account.PayInFunds(depositValue);
+                break;
+            }
+        }
+        public void WithdrawFunds()
+        {
+            decimal withdrawValue;
+            Console.WriteLine("\n-----Withdraw Funds-----");
+            while (true)
+            {
+                withdrawValue = CustomerAccount.ValidateDecimal("Enter how much money you would like to withdraw: ", 0, 1000);
+                if (account.WithdrawFunds(withdrawValue))
+                    break;
+            }
+
+        }
+        public static string ThisOrThat(string prompt, string errorMsg, string answerOne, string answerTwo)
+        {
+            if (answerOne == answerTwo)
+            {
+                //throw an exception that says your two answers are the same
+            }
+            string answer;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                answer = AccountEditUI.TrimLower(Console.ReadLine());
+                if (answer != answerOne && answer != answerTwo)
+                {
+                    Console.WriteLine(errorMsg);
+                    continue;
+                }
+                return answer;
+            }
+        }
+        public static string TrimLower(string toFormat)
+        {
+            toFormat = toFormat.Trim();
+            toFormat = toFormat.ToLower();
+            return toFormat;
+        }
+    }
+    public class BankException : Exception
+    {
+        public BankException(String message) :
+            base(message)
+        {
+        }
+    }
+    class BankProgram
+    {
+
+        public static void Main()
+        {
+            string input;
+            Console.WriteLine("------Welcome to the bank------\n\n\n");
+            DictionaryBank ourBank = DictionaryBank.Load("Test.txt");
+
+            if (!ourBank.AccountsInBank()) //at the moment, if this tries to load an empty document, it returns a null value and throws an exception that i cant do anything with. Might need to try a "TRYCATCH"  
+            {
+                //
+            }
+            do
+            {
+                Console.WriteLine("Would you like to create a new account or edit an existing one?\n" +
+                    "Type new to create an account or type edit to edit an existing account (to exit, type exit).");
+                input = AccountEditUI.TrimLower(Console.ReadLine());
+                switch (input)
+                {
+                    case "new":
+                        AccountCreateUI.CreateScript(ourBank);
+                        break;
+                    case "edit":
+                        AccountEditUI.EditScript(ourBank);
+                        break;
+
+                }
+            } while (input != "exit");
+
+
+            //CustomerAccount newAccount = new CustomerAccount("Rob", 10000);
+
+            //if (ourBank.StoreAccount(newAccount))
+            //    Console.WriteLine("CustomerAccount added to bank with account number: " + newAccount.GetAccountNumber());
+
+            //BabyAccount newBabyAccount = new BabyAccount("David", 100, "Rob");
+
+            //if (ourBank.StoreAccount(newBabyAccount))
+            //    Console.WriteLine("BabyAccount added to bank with account number: " + newBabyAccount.GetAccountNumber());
+            //Console.WriteLine();
+
+            ourBank.Save("Test.txt");
+
+
+            //DictionaryBank loadBank = DictionaryBank.Load("Test.txt");
+
+            //IAccount storedAccount = loadBank.FindAccount("David");
+            //if (storedAccount != null)
+            //{
+            //    Console.WriteLine(storedAccount.GetType() + " found in bank");
+            //}
+            //storedAccount = loadBank.FindAccount("Jack");
+            //if (storedAccount != null)
+            //{
+            //    Console.WriteLine(storedAccount.GetType() + " found in bank");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
+
